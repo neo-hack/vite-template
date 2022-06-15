1c4efc596f076cc56038d4370600e9ab26537a10
JiangWeixian <jiangweixian1994@gmail.com>
2022-06-15 09:27:15 +0800

    :sparkles:  plugin-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
 import React from 'react'
-import { RouteObject, useRoutes, BrowserRouter } from 'react-router-dom'
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import routes from '~react-pages'
 
 import { ThemeProvider } from '@/styles/theme'
-import NotFound from './pages/not-found'
-
-const Demo = React.lazy(() => import('@/pages/demo'))
-const Home = React.lazy(() => import('@/pages/home'))
-
-const routes: RouteObject[] = [
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/demo',
-    element: <Demo />,
-  },
-  {
-    path: '*',
-    element: <NotFound />,
-  },
-]
-
-const Routes = () => {
-  const elements = useRoutes(routes)
-  return elements
-}
 
 function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <React.Suspense fallback={<div>loading...</div>}>
-          <Routes />
-        </React.Suspense>
+        <React.Suspense fallback={<div>loading...</div>}>{useRoutes(routes)}</React.Suspense>
       </BrowserRouter>
     </ThemeProvider>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pages from 'vite-plugin-pages'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pages()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
