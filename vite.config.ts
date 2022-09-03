3ac12acf12bc0adeec7b1ffa1ed2cac59a9faca1
JiangWeixian <jiangweixian1994@gmail.com>
2022-09-03 11:31:29 +0800

    :sparkles: add plugin doc

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import pages from 'vite-plugin-pages'
+import { VitePluginDocument } from 'vite-plugin-document'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pages()],
+  plugins: [react(), pages(), VitePluginDocument()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
