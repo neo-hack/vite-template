adc28b72f180fc5e466acf257a03439b91e9d1c1
JW <jiangweixian1994@gmail.com>
2023-05-13 21:15:41 +0800

    :sparkles: import svg as component

---
+++ .changeset/thin-items-drop.md
@@ -0,0 +1,5 @@
+---
+"@aiou/vite-template": patch
+---
+
+upgrade eslint & automatic-jsx
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'node:path'
 
+import { svgrs } from '@svgr-rs/svgrs-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePluginDocument } from 'vite-plugin-document'
@@ -8,7 +9,7 @@ import pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pages(), VitePluginDocument(), !!process.env.VITE_INSPECT && inspect()],
+  plugins: [react(), pages(), svgrs(), VitePluginDocument(), !!process.env.VITE_INSPECT && inspect()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
